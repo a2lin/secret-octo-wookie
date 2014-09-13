9b729a6a16f7fad17939a6bb7e933c8fd8f51b94
Aditya Majumdar <aditya29@gmail.com>
2014-09-13 10:31:24 -0400

    wav file split into partitions

--- dsptest.py
+++ dsptest.py
@@ -1,37 +1,47 @@
 import numpy as np
 # import matplotlib.pyplot as plt
-from scipy import signal
+# from scipy import signal
 import wave
 import sys
 
-spf = wave.open('test3.wav','rb')
-print "commiting suicide now"
+input_file = "test2.wav"
+spf = wave.open(input_file,'rb')
 
 # Extract Raw Audio from Wav File
 waveform = spf.readframes(-1)
-waveform = np.fromstring(waveform, 'Int16')[44100*10 : 44100*20]
+#print waveform
+#len(waveform) / 44100 = #seconds in .wav file?
+#time = len(waveform) / spf.getframerate()
 
-# What the fuck am I doing
-s_rate = spf.getframerate()
-n_rate = s_rate/2
-v_l_rate = 100
-v_h_rate = 800
-v_l_ratio = float(v_l_rate)/n_rate
-v_h_ratio = float(v_h_rate)/n_rate
+cpf = len(waveform) / spf.getnframes()
+adjusted_sample_rate = spf.getframerate() * cpf
 
-b, a = signal.butter(5, v_h_ratio, 'lowpass')
-vocals = signal.filtfilt(b, a, waveform)
+time = len(waveform) / adjusted_sample_rate
+#time = len(waveform) / 44100
+print "time is " + str(time)
+#where 44100 is the frame rate
 
-spf2 = wave.open('test4.wav','wb')
-spf2.setparams(spf.getparams())
-spf2.setnframes(len(vocals))
-spf2.writeframes(vocals)
+partition = 10
 
-spf.close()
-spf2.close()
+numCycles = time/partition
+print "numCycles is " + str(numCycles)
+int1 = 0
+int2 = partition
 
-#plt.plot(np.absolute(np.fft.fft(waveform)))
-#plt.plot(np.absolute(np.fft.fft(vocals)))
-#plt.show()
+for num in range(1, numCycles):
+	if num > 1:
+		spf = wave.open(input_file,'rb')
+		waveform = spf.readframes(-1)
 
-print "fuck fuck fuck fucking fuck fuck fucking fuck of fucking fuck"
+	vocals = waveform[adjusted_sample_rate*int1 : adjusted_sample_rate*int2]
+
+	spf2 = wave.open('part' + str(num) + '.wav','wb')
+	spf2.setparams(spf.getparams())
+	spf2.setnframes(len(vocals))
+	spf2.writeframes(vocals)
+
+	int1 += partition
+	int2 += partition
+
+	spf.close()
+	spf2.close()
