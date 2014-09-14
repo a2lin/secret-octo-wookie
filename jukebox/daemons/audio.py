721e01ab18ff072d8f9b2c2dd2ebdfe756682a11
Xander Lin <ajlin@mit.edu>
2014-09-13 21:57:21 -0400

    more file path fixes

--- jukebox/daemons/audio.py
+++ jukebox/daemons/audio.py
@@ -5,9 +5,10 @@ from firebase import Firebase
 import time
 import random
 import json
+import os
 
-from jukebox.lib.audio_io import slice_wav
-from jukebox.mix import main as mix
+import jukebox.lib.audio_io
+import jukebox.mix
 
 WRITE_TOKEN = "hXMjeLwYHyGMZQiVmNjueFxwvGoGEAZshpNFUNVG"
 FIREBASE_URL = "https://blazing-fire-4446.firebaseio.com/"
@@ -23,49 +24,38 @@ class AudioDaemon(Thread):
         while(1):
             start_time = time.time()
 
-            songs = self.pick_songs(2);
-            files = [self.slice_song(song) for song in songs]
-            mashup, key = self.collide_songs(files);
+            song_ids = self.get_next();
+            files = [self.slice_song(id) for id in song_ids]
+            mashup = self.collide_songs(files);
 
 
             self.schedule(mashup) 
             sleep(25 - (time.time() - start_time))
 
     # Return two biased randomly selected song id's from firebase
-    def pick_songs(self, count):
-        biased_indices = []
-        result_songs = []
-
-        songs = Firebase(FIREBASE_URL + "songs").get()
-
-        for song in songs:
-           biased_indices += [song["id"]] * song["rng_bias"]
-
-        for i in range(count):
-            new_index = biased_indices[int(len(biased_indices) * random.random())]
-
-            result_songs += [new_index]
-            biased_indices = [bi for bi in biased_indices if bi is not new_index]
-
-        return result_songs
+    # TODO add grab next logic
+    def get_next(self):
+        return ["0", "1"] 
 
     # Returns the file name of a song slice 
     def slice_song(self, song_id):
-        song = json.load(Firebase(FIREBASE_URL + "songs/" + song_id).get())
+        song = Firebase(FIREBASE_URL + "songs/" + song_id).get()
 
         file_name = song['file_name']
         start_time = song['start_time']
 
-        new_file = wavslice + "_" + song_id + "_" + time.time() + ".wav"
-        new_path = os.getcwd() + "/../tmp/" + new_file
+        new_file = "slice_wav_" + song_id + "_" + str(time.time()) + ".wav"
+        new_path = os.getcwd() + "/tmp/" + new_file
 
-        slice_wave(file_name, new_path, start_time, start_time + MIX_DURATION) 
+        jukebox.lib.audio_io.slice_wav(file_name, new_path, start_time, start_time + MIX_DURATION) 
+
+        return new_path
 
     # Collides two wavs file1 and file2. Returns path to crushed file.
     # TODO make better collider
     def collide_songs(self, files):
-        target_file = os.getcwd() + "/../static/data/330.wav"
-        main(40, files, target_file)
+        target_file = os.getcwd() + "/static/data/330.wav"
+        jukebox.mix.main(40, files, target_file)
         return target_file
 
     # TODO make better scheduler
---
+++ tmp/readme.txt
@@ -0,0 +1,1 @@
+Storage space for audio slices.
